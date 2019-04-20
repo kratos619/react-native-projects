b93401c13171a822a2e594736dc21f9f1df97a26
kratos619 <gaurav.palaspagar@hotmail.com>
2019-04-20 12:45:44 +0530

    text input two way binding

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Gaurav</Text>
+        <Text>Gaurav Milind Palaspagar new</Text>
+        <TextInput
+          style={{ width: 300, borderColor: 'black', borderWidth: 1 }}
+          onChangeText={this.placeNameChangeHandler}
+          value={this.state.placeName}
+        />
       </View>
     );
   }
@@ -16,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
