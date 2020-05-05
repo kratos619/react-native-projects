e886c587b249b6db55ecd0a6c7a261b9674ec97f
kratos619 <gaurav.palaspagar@hotmail.com>
2020-05-05 11:41:46 +0530

    update to 37

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Avatar } from 'react-native-elements';
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World </Text>
+        <Avatar
+          rounded
+          source={{
+            uri:
+              'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+          }}
+        />
       </View>
     );
   }
